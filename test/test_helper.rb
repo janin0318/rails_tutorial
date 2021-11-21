c4c62679df605ecc39c6c3ccae953be623b02a5e
janin0318 <janin-ou@outlook.jp>
2021-11-22 00:33:31 +0900

    Finish user edit, update, index, and destroy actions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 require "minitest/reporters"
-Minitest::Reporters.use!
+Minitest::Reporters.use! if ENV['RM_INFO']
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
